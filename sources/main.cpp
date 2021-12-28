d6b44d9c5c45099fcce071ffde9f59e6c71f6cd9
Lasar1k <alf.ivan2002@gmail.com>
2021-12-28 13:34:17 -0800

    some fixes

--- sources/main.cpp
+++ sources/main.cpp
@@ -19,25 +19,24 @@ int main(int argc, char* argv[])
   size_t count_of_threads = 0;
   if (argc >= 2)
   {
-	  count_of_threads = boost::lexical_cast<size_t>(argv[1]);
-  }
-  else
+    count_of_threads = boost::lexical_cast<size_t>(argv[1]);
+  } else
   {
-		count_of_threads = std::thread::hardware_concurrency();
+    count_of_threads = std::thread::hardware_concurrency();
   }
 
   std::vector<std::thread> threads;
-	threads.reserve(count_of_threads);
+  threads.reserve(count_of_threads);
 
-	for (size_t index = 0; index < count_of_threads; ++index)
-	{
-		threads.emplace_back(HashSearcher::SearchHash);
-	}
+  for (size_t index = 0; index < count_of_threads; ++index)
+  {
+    threads.emplace_back(HashSearcher::SearchHash);
+  }
 
-	for (std::thread& thread : threads)
-	  {
-		thread.join();
-	  }
+  for (std::thread& thread : threads)
+    {
+    thread.join();
+    }
 
-	return 0;
+  return 0;
 }
