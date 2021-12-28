44f353f7369a869a7cdd13af8ad6b99ccdcc3f5d
Lasar1k <alf.ivan2002@gmail.com>
2021-12-28 13:30:15 -0800

    some code

--- include/HashSearcher.hpp
+++ include/HashSearcher.hpp
@@ -1,15 +1,14 @@
 // Copyright 2021 Lasar1k <alf.ivan2002@gmail.com>
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_HASHSEARCHER_HPP_
+#define INCLUDE_HASHSEARCHER_HPP_
 
 #include <picosha2.h>
 #include <boost/log/trivial.hpp>
 
-class HashSearcher 
-	{
-	 public:
-	  static void SearchHash();
-	};
+class HashSearcher {
+  public:
+  static void SearchHash();
+};
 
-#endif // INCLUDE_EXAMPLE_HPP_
+#endif // INCLUDE_HASHSEARCHER_HPP_
--- sources/main.cpp
+++ sources/main.cpp
@@ -7,38 +7,37 @@
 #include "HashSearcher.h"
 
 void init()
-    {
-        boost::log::add_file_log("Log.log");
-        srand(time(nullptr));
-        boost::log::add_console_log(std::cout);
-    }
+{
+  boost::log::add_file_log("Log.log");
+  srand(time(nullptr));
+  boost::log::add_console_log(std::cout);
+}
 
 int main(int argc, char* argv[])
+{
+  init();
+  size_t count_of_threads = 0;
+  if (argc >= 2)
+  {
+	  count_of_threads = boost::lexical_cast<size_t>(argv[1]);
+  }
+  else
+  {
+		count_of_threads = std::thread::hardware_concurrency();
+  }
+
+  std::vector<std::thread> threads;
+	threads.reserve(count_of_threads);
+
+	for (size_t index = 0; index < count_of_threads; ++index)
 	{
-	    init();
-	  size_t count_of_threads = 0;
-	  if (argc >= 2) 
-		  {
-			count_of_threads = boost::lexical_cast<size_t>(argv[1]);
-		  } 
-	  else 
-		  {
-			count_of_threads = std::thread::hardware_concurrency();
-		  }
-
-	  std::vector<std::thread> threads;
-	  threads.reserve(count_of_threads);
-
-	  for (size_t index = 0; index < count_of_threads; ++index) 
-		  {
-			threads.emplace_back(HashSearcher::SearchHash);
-		  }
-
-	  for (std::thread& thread : threads) 
-		  {
-			thread.join();
-		  }
-	  
-	  return 0;
+		threads.emplace_back(HashSearcher::SearchHash);
 	}
-  
+
+	for (std::thread& thread : threads)
+	  {
+		thread.join();
+	  }
+
+	return 0;
+}
